6f78155c14722bbfaca9c9a565d4bc981db91ad3
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-03-20 16:31:00 +0600

    Serving JSPs with Jar Packaging

---
+++ logigbig7/src/main/java/com/logicbig/example/WebMvcConfigExample.java
@@ -0,0 +1,13 @@
+package com.logicbig.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMvcConfigExample {
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(WebMvcConfigExample.class);
+        app.run(args);
+    }
+}
---
+++ logigbig7/target/classes/META-INF/resources/WEB-INF/views/myView.jsp
@@ -0,0 +1,7 @@
+<h2>From JSP page</h2>
+<%@ page language="java" %>
+<html>
+<body>
+Message : ${msg}
+</body>
+</html>
