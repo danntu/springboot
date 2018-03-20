baf742042b352d2f41bea4270c6fa76ae2c14559
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-03-20 14:31:16 +0600

    Setting Cache Period for Static Resources

---
+++ logicbig6/src/main/java/ExampleMain.java
@@ -0,0 +1,10 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAutoConfiguration
+public class ExampleMain{
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleMain.class,args);
+    }
+}
